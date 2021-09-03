a29ea27d040c2f0ee0a8cfaadfce7fcf72d8157d
JessicaLima <jessikurt@gmail.com>
2021-09-03 14:22:42 -0300

    created Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+
+import { Component, Head } from "./styles";
+import Logo from '../../assets/image/pokeicon.png';
+
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <Component>
+      <Head>
+        <h2>JessyMon</h2>
+        <img src={Logo} alt="Icone de uma Pokebola" />
+      </Head>
+    </Component>
+  );
+};
 
 export default Header;
