56e99aaa4b7b34281f020d1ebb2a94e5a71ba9a6
JessicaLima <jessikurt@gmail.com>
2021-09-08 12:50:14 -0300

    styled:created the global style for the components

---
+++ src/styles/global.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+
+import PokemonLight from "../../src/assets/font/PokemonHollow.ttf";
+import PokemonBold from "../../src/assets/font/PokemonSolid.ttf";
+
+const Global =  createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+  
+  @font-face {
+    font-family: PokemonLight;
+    src: url(${PokemonLight});
+  }
+  @font-face {
+    font-family: PokemonBold;
+    src: url(${PokemonBold});
+  }
+`;
+
+export default Global;
