4c644fb440e86028b9632ff6faf862f1cab90300
JessicaLima <jessikurt@gmail.com>
2021-09-08 14:02:21 -0300

    docs: separated the ListItem for the isolated component pokecard

--- src/components/ListPokeSection/styles.js
+++ src/components/ListPokeSection/styles.js
@@ -6,69 +6,15 @@ export const Component = styled.div`
 `;
 
 export const Section = styled.div`
-  background-color: gainsboro;
-  height: 600px;
-  width: 70vw;
+  width: 45vw;
   border-radius: 4px;
   display: grid;
 `;
 
 export const List = styled.div`
   display: grid;
-  grid-gap: 2rem;
-  background: blue;
+  grid-gap: 1rem;
   grid-template-columns: 1fr 1fr 1fr;
-  grid-template-rows: 200px 200px 200px;
+  grid-template-rows: 250px 250px 250px;
 `;
 
-export const PokeOption = styled.div`
-  background: pink;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  img {
-    background: white;
-    border-radius: 50%;
-    padding: 4px;
-    width: 40%;
-  }
-`;
-
-export const Listq = styled.div`
-  list-style: none;
-  background: goldenrod;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, 100px);
-  grid-template-rows: 50px 50px 50px 50px;
-  grid-gap: 2rem;
-  /*
-  div {
-    width: 150px;
-    height: 180px;
-    border-radius: 4px;
-    letter-spacing: 2px;
-  }*/
-
-  img {
-    background: white;
-    border-radius: 50%;
-    padding: 5px;
-  }
-`;
-
-export const Description = styled.div`
-  height: 90px;
-  width: 100%;
-  color: white;
-  letter-spacing: 4px;
-  font-family: PokemonBold;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  button {
-    cursor: pointer;
-    border: 0;
-    background: pink;
-    color: white;
-  }
-`;
