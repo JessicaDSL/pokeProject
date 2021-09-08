2281b7d9f2e193551b73650cf3c85212115e4784
JessicaLima <jessikurt@gmail.com>
2021-09-08 18:26:35 -0300

    refactor: add new component

--- src/components/ListPokeSection/ListPokeSection.js
+++ src/components/ListPokeSection/ListPokeSection.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 import { Section, PokeSection } from "./styles";
-import PokeCard from "../../PokeCard";
+import PokeCard from "../PokeCard";
 
 const ListPokeSection = () => {
   return (
     <Section>
       <PokeSection>
+           <PokeCard /> 
+           <PokeCard /> 
+           <PokeCard /> 
+           <PokeCard /> 
+           <PokeCard /> 
+           <PokeCard /> 
+           <PokeCard /> 
+           <PokeCard /> 
            <PokeCard />    
       </PokeSection>
     </Section>
