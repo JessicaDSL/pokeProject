0741fb9320e8a3d143f0170f71d64837be1316b3
JessicaLima <jessikurt@gmail.com>
2021-09-08 18:10:24 -0300

    docs: created PokeCard component

--- src/components/PokeCard/content.js
+++
@@ -1,9 +0,0 @@
-export const list = [
-  {
-    image:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
-    ids: 167,
-    name: "venusaur",
-    type: "poison",
-    species: "venusaur"
-  }
-]
