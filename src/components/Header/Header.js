ced81dfe94108d505f818f82ede83fc3dbc8215d
JessicaLima <jessikurt@gmail.com>
2021-09-10 17:19:27 -0300

    refactor: refactored Header Component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,15 @@
 import React from "react";
 
-import { Component, Head } from "./styles";
+import { Background } from "./styles";
 import Logo from '../../assets/image/pokeicon.png';
 
 
 const Header = () => {
   return (
-    <Component>
-      <Head>
-        <h2>JessyMon</h2>
+    <Background>
+        <h2>PokeJéssy</h2>
         <img src={Logo} alt="Icone de uma Pokebola" />
-      </Head>
-    </Component>
+    </Background>
   );
 };
 
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,24 +1,22 @@
 import styled from "styled-components";
 
-export const Component = styled.div`
+export const Background = styled.div`
   height: 110px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-around;
   color: #fff;
   background: linear-gradient(to right, #f1305a 20%, transparent 100%);
-  font-family: PokemonHollow;
+  font-family: PokemonLight;
   letter-spacing: 4px;
 
+  h2{
+    font-size: 3rem;
+    margin-bottom: 1rem;
+  }
   img {
     width: 5rem;
     cursor: pointer;
+    margin-left: 20px;
   }
-`;
-
-export const Head = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  margin: 50px;
-`;
+`
