d30451cbbd2f5bcb5a1cceb839c481bb6689437a
JessicaLima <jessikurt@gmail.com>
2021-09-10 18:36:36 -0300

    change ids for id parameter

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,10 @@
-export function verifyId(item) {
-  if (item.ids <= 9) {
-    return `#00${item.ids}`;
-  } else if (item.ids >= 10 && item.ids <= 99) {
-    return `#0${item.ids}`;
+export function formatPokeId(item) {
+  if (item.id <= 9) {
+    return `#00${item.id}`;
+  } else if (item.id >= 10 && item.id <= 99) {
+    return `#0${item.id}`;
   } else {
-    return `#${item.ids}`;
+    return `#${item.id}`;
   }
 }
 
