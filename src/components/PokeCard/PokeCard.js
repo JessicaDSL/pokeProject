acb165df702e3192937508386cd1d52f33976f99
JessicaLima <jessikurt@gmail.com>
2021-09-10 18:33:21 -0300

    refactor: change function for loop

--- src/components/PokeCard/PokeCard.js
+++ src/components/PokeCard/PokeCard.js
@@ -2,32 +2,30 @@ import React from "react";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 
 import { ListItem, Description } from "./styles";
-import { cardInfo } from "./content";
-import { verifyId } from "../../utils/utils";
+import { formatPokeId } from "../../utils/utils";
 
-const PokeCard = () => {
+const PokeCard = ({ image, id }) => {
   return (
-    <>
-      {cardInfo.map((item, key) => {
-        return (
-          <ListItem key={key}>
-            <div>
-              <img src={item.image} alt="" />
-            </div>
-            <Description>
-              <span>{verifyId(item)}</span>
-              <button>
-                <FavoriteBorderIcon fontSize="large" />
-              </button>
-            </Description>
-          </ListItem>
-        );
-      })}
-    </>
+    <ListItem>
+      <div>
+        <img src={image} alt="" />
+      </div>
+      <Description>
+        <span>{formatPokeId({id})}</span>
+        <button>
+          <FavoriteBorderIcon fontSize="large" />
+        </button>
+      </Description>
+    </ListItem>
   );
 };
 
 export default PokeCard;
+
+/*
+              
+              
+              */
 
 /*<ListItem key={key}>
                 <div>
