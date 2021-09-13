b363cb414ef847aedb6fdd9298e88895507e67db
JessicaLima <jessikurt@gmail.com>
2021-09-13 18:34:47 -0300

    feat: created TeamCard component

---
+++ src/components/TeamCard/TeamCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import DeleteIcon from '@material-ui/icons/Delete';
+import { Container } from './styles'
+
+const TeamCard = ({image, alt}) => {
+  return(
+    <Container>
+      <img src={image} alt={alt} />
+      <button>
+        <DeleteIcon />
+      </button>
+    </Container>
+  )
+}
+
+export default TeamCard;
---
+++ src/components/TeamCard/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+background-color: #fff;
+width: 9rem;
+border-radius: 50%;
+button {
+  color: white;
+  border-radius: 50%;
+  padding: 4px;
+  border: 0;
+  background-color: rgba(0, 0, 0, .2);
+}
+`
