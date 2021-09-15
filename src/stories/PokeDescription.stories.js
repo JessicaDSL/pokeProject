ce64199e50eae4bbfd264b8aaf9a25c644984b6f
JessicaLima <jessikurt@gmail.com>
2021-09-15 13:52:14 -0300

    feat: change PokeDescription for PokeItem component

--- src/stories/PokeDescription.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PokeDescription from '../components/PokeDescription';
-
-export default {
-  title: 'Components/PokeDescription',
-  component: PokeDescription,
-}
-
-export const component = () => <PokeDescription />
