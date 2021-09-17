e014bfc7868228418bf63504127d481c13a89601
JessicaLima <jessikurt@gmail.com>
2021-09-17 12:36:07 -0300

    export const for a mock

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,65 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import { Container } from './styles';
-import Team from '../../components/Team'
-import PokeList from '../../components/PokeList'
-import Footer from '../../components/Footer'
+import { Container } from "./styles";
+import { favouritedPokemons, pokemons } from "./mock";
+import Team from "../../components/Team";
+import PokeList from "../../components/PokeList";
+import Footer from "../../components/Footer";
 
 const Home = () => {
-  const favouritedPokemons = [
-    {
-      image:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
-      id: 167,
-      name: "venusaur",
-      type: "poison",
-      species: "venusaur",
-    },{
-      image:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
-      id: 167,
-      name: "venusaur",
-      type: "poison",
-      species: "venusaur",
-    },{
-      image:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
-      id: 167,
-      name: "venusaur",
-      type: "poison",
-      species: "venusaur",
-    },{
-      image:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
-      id: 167,
-      name: "venusaur",
-      type: "poison",
-      species: "venusaur",
-    },{
-      image:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
-      id: 167,
-      name: "venusaur",
-      type: "poison",
-      species: "venusaur",
-    },{
-      image:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
-      id: 167,
-      name: "venusaur",
-      type: "poison",
-      species: "venusaur",
-    }
-  ];
-  
-  return(
+  return (
     <Container>
-      {console.log({favouritedPokemons})}
       <Team favouritedPokemons={favouritedPokemons} />
-      <PokeList favouritedPokemons={favouritedPokemons} />
+      <PokeList pokemons={pokemons} />
       <Footer />
     </Container>
   );
-}
+};
 
 export default Home;
