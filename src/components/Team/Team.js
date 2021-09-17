0be8fb5b4d9b915252775604b99a2790544f1fab
JessicaLima <jessikurt@gmail.com>
2021-09-17 11:16:38 -0300

    included favouritedPokemons parameter

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -3,15 +3,16 @@ import React from "react";
 import { Container, List, ListItem } from "./styles";
 import TeamCard from "../TeamCard";
 
-const Team = ({ pokemon = [] }) => {
+const Team = ({favouritedPokemons}) => {
   return (
     <Container>
       <h1>Meu time</h1>
+      {favouritedPokemons.image}
       <List>
         <ListItem>
-          {pokemon.map(({image, alt}) => (
-            <TeamCard image={image} alt={alt} />
-          ))}
+          {favouritedPokemons.map((pokemon) => 
+                <TeamCard image={pokemon.image} alt={pokemon.alt} />
+          )}
         </ListItem>
       </List>
     </Container>
@@ -19,3 +20,6 @@ const Team = ({ pokemon = [] }) => {
 };
 
 export default Team;
+/* {favouritedPokemons.map(({image, alt}) => (
+            <TeamCard image={image} alt={alt} />
+          ))}*/
