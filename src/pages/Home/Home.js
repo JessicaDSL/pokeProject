556255fba8cdd2cc5efa623865395644fdcca58a
JessicaLima <jessikurt@gmail.com>
2021-09-17 11:14:00 -0300

    feat: included data with favouritedPokemons const

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,65 @@
 import React from 'react';
 
 import { Container } from './styles';
+import Team from '../../components/Team'
+import PokeList from '../../components/PokeList'
+import Footer from '../../components/Footer'
 
 const Home = () => {
-  return <Container>Home</Container>;
+  const favouritedPokemons = [
+    {
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+      id: 167,
+      name: "venusaur",
+      type: "poison",
+      species: "venusaur",
+    },{
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+      id: 167,
+      name: "venusaur",
+      type: "poison",
+      species: "venusaur",
+    },{
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+      id: 167,
+      name: "venusaur",
+      type: "poison",
+      species: "venusaur",
+    },{
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+      id: 167,
+      name: "venusaur",
+      type: "poison",
+      species: "venusaur",
+    },{
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+      id: 167,
+      name: "venusaur",
+      type: "poison",
+      species: "venusaur",
+    },{
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+      id: 167,
+      name: "venusaur",
+      type: "poison",
+      species: "venusaur",
+    }
+  ];
+  
+  return(
+    <Container>
+      {console.log({favouritedPokemons})}
+      <Team favouritedPokemons={favouritedPokemons} />
+      <PokeList favouritedPokemons={favouritedPokemons} />
+      <Footer />
+    </Container>
+  );
 }
 
 export default Home;
