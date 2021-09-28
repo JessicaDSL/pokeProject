cb55dd904c1a49c7b98ab167f33eddf34d1f410c
JessicaLima <jessikurt@gmail.com>
2021-09-28 12:25:57 -0300

    feat: increase new functions on utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,11 +1,21 @@
-export function formatPokeId(item) {
-  if (item.id <= 9) {
-    return `#00${item.id}`;
-  } else if (item.id >= 10 && item.id <= 99) {
-    return `#0${item.id}`;
+export function formatPokeId(id) {
+  if (id <= 9) {
+    return `#00${id}`;
+  } else if (id >= 10 && id <= 99) {
+    return `#0${id}`;
   } else {
-    return `#${item.id}`;
+    return `#${id}`;
   }
 }
 
-export const pokeImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png"
+export const formatPokeList = ({ name, url }) => {
+  const [, , , , , , id] = url.split("/");
+  const image = formatImage(id);
+  return { id, name, image };
+};
+
+export const formatImage = (id) =>
+  `https://cdn.traction.one/pokedex/pokemon/${id}.png`;
+
+export const pokeImage =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png";
