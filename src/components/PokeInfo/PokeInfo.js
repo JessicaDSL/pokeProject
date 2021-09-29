4bdf325c1a34435dfd6a4dc18e8d20daa0db92cd
JessicaLima <jessikurt@gmail.com>
2021-09-29 12:12:46 -0300

    feat: change const ability for abilities and type for types

--- src/components/PokeInfo/PokeInfo.js
+++ src/components/PokeInfo/PokeInfo.js
@@ -4,11 +4,11 @@ import PokeDescription from "../PokeDescription";
 
 import { Background } from "./styles";
 
-const PokeInfo = ({ image, id, name, ability, type }) => {
+const PokeInfo = ({ image, id, name, abilities, types }) => {
   return (
     <Background>
       <PokeImage image={image} id={id} alt={`Foto do pokemon ${name}`} />
-      <PokeDescription name={name} type={type} ability={ability} />
+      <PokeDescription name={name} types={types} abilities={abilities} />
     </Background>
   );
 };
