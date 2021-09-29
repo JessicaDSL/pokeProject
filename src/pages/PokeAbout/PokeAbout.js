14ab0d92183f4bbef4ee96815471563202ff5182
JessicaLima <jessikurt@gmail.com>
2021-09-29 12:10:49 -0300

    feat: change const ability for abilities and type for types

--- src/pages/PokeAbout/PokeAbout.js
+++ src/pages/PokeAbout/PokeAbout.js
@@ -18,20 +18,11 @@ const PokeAbout = () => {
         const name = data?.name;
         const ids = data?.id;
         const image = `https://cdn.traction.one/pokedex/pokemon/${id}.png`;
-        const ability = data?.abilities
-          ?.map((item) => {
-            const pokeDateAbility = item?.ability?.name;
-            return pokeDateAbility;
-          })
+        const abilities = data?.abilities
+          ?.map((item) => item?.ability?.name)
           .join(", ");
-        const type = data?.types
-          ?.map((item) => {
-            const pokeDateType = item?.type?.name;
-            return pokeDateType;
-          })
-          .join(", ");
-        console.log(ability);
-        return setPokeList({ name, ids, image, ability, type });
+        const types = data?.types?.map((item) => item?.type?.name).join(", ");
+        return setPokeList({ name, ids, image, abilities, types });
       })
       .catch((err) => `deu ruim amore: ${err}`);
   }, []);
@@ -43,20 +34,11 @@ const PokeAbout = () => {
         image={pokeList.image}
         id={pokeList.ids}
         name={pokeList.name}
-        ability={pokeList.ability}
-        type={pokeList.type}
+        abilities={pokeList.abilities}
+        types={pokeList.types}
       />
     </Container>
   );
 };
 
 export default PokeAbout;
-/*const pokeData = data?.map((item) => {
-        //const [, , , , , , id] = url.split("/");
-        const name = item.name;
-        const type = item.type;
-        const specie = item.specie;
-        
-        return { name, type, specie };
-       
-      });*/
