0ad64ad4f5e2127f58aa877b4c6e45a5765db0b8
JessicaLima <jessikurt@gmail.com>
2021-10-13 14:35:00 -0300

    style: change width for image

--- src/components/TeamCard/styles.js
+++ src/components/TeamCard/styles.js
@@ -1,14 +1,17 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-background-color: #fff;
-width: 9rem;
-border-radius: 50%;
-button {
-  color: white;
+  background-color: #fff;
+  width: 9rem;
   border-radius: 50%;
-  padding: 4px;
-  border: 0;
-  background-color: rgba(0, 0, 0, .2);
-}
-`
+  img {
+    width: 150px;
+  }
+  button {
+    color: white;
+    border-radius: 50%;
+    padding: 4px;
+    border: 0;
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+`;
