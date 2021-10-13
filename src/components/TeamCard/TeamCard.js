fdb5596c69c6be28d574d58534ac40635bde87c7
JessicaLima <jessikurt@gmail.com>
2021-10-13 18:28:06 -0300

    feat: add handleDelete function on button

--- src/components/TeamCard/TeamCard.js
+++ src/components/TeamCard/TeamCard.js
@@ -2,11 +2,11 @@ import React from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { Container } from "./styles";
 
-const TeamCard = ({ image, alt }) => {
+const TeamCard = ({ image, alt, handleDelete }) => {
   return (
     <Container>
       <img src={image} alt={alt} />
-      <button>
+      <button onClick={handleDelete}>
         <DeleteIcon />
       </button>
     </Container>
