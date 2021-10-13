6a4f8fd8d460fd5173c3badfb186ee6feb7f062e
JessicaLima <jessikurt@gmail.com>
2021-10-13 14:31:54 -0300

    feat: fix indentation

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -4,16 +4,13 @@ import { Container, List, ListItem } from "./styles";
 import TeamCard from "../TeamCard";
 
 const Team = ({ pokemons = [] }) => {
-
-  
-
   return (
     <Container>
       <h1>Meu time</h1>
       <List>
         <ListItem>
           {pokemons.map((pokemon, key) => (
-            <TeamCard image={pokemon.image} alt={pokemon.alt} key={key}/>
+            <TeamCard image={pokemon.image} alt={pokemon.alt} key={key} />
           ))}
         </ListItem>
       </List>
