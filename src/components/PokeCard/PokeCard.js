e1e7a3a3231c41974f46fc302306a2215e4d7252
JessicaLima <jessikurt@gmail.com>
2021-10-13 14:24:53 -0300

    feat: add function handleSelect for button

--- src/components/PokeCard/PokeCard.js
+++ src/components/PokeCard/PokeCard.js
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 
 import { Container, Description } from "./styles";
 
-const PokeCard = ({ image, id }) => {
+const PokeCard = ({ image, id, handleSelect }) => {
   return (
     <Container>
-        <Link to={`/pokeabout/${id}`}>
-          <img src={image} alt="" />
-        </Link>
+      <Link to={`/pokeabout/${id}`}>
+        <img src={image} alt="" />
+      </Link>
       <Description>
         <span>{id}</span>
-        <button>
+        <button onClick={handleSelect}>
           <FavoriteBorderIcon fontSize="large" />
         </button>
       </Description>
