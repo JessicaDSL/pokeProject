e7023fa8149cb1d6c808e55d9637573a6d2714b7
JessicaLima <jessikurt@gmail.com>
2021-10-18 17:40:42 -0300

    feat: included description div

--- src/components/TeamCard/TeamCard.js
+++ src/components/TeamCard/TeamCard.js
@@ -1,14 +1,17 @@
 import React from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
-import { Container } from "./styles";
+import { Container, Description } from "./styles";
 
-const TeamCard = ({ image, alt, handleDelete }) => {
+const TeamCard = ({ image, alt, name, handleDelete }) => {
   return (
     <Container>
       <img src={image} alt={alt} />
-      <button onClick={handleDelete}>
-        <DeleteIcon />
-      </button>
+      <Description>
+        <h3>{name}</h3>
+        <button onClick={handleDelete}>
+          <DeleteIcon />
+        </button>
+      </Description>
     </Container>
   );
 };
