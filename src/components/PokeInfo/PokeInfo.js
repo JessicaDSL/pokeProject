08c029f6a9f1a57bcb8aa4e3655fc0dfc2e7def4
JessicaLima <jessikurt@gmail.com>
2021-10-27 17:10:25 -0300

    feat: included div

--- src/components/PokeInfo/PokeInfo.js
+++ src/components/PokeInfo/PokeInfo.js
@@ -1,18 +1,18 @@
 import React from "react";
 
-import PokeDescription from '../PokeDescription'
+import PokeDescription from "../PokeDescription";
 import PokeStatus from "../PokeStatus";
 
-import { Container, Background, Image} from './styles'
+import { Container, Background, Image, Box } from "./styles";
 
 const PokeInfo = ({ pokeList = [], status = [] }) => {
-
   return (
     <Container>
       <Background>
         <PokeDescription pokeList={pokeList} />
         <PokeStatus status={status} />
       </Background>
+      <Box></Box>
       <Image>
         <img src={pokeList.image} alt={pokeList.name} />
       </Image>
