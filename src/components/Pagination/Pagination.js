278515fc842c1969014edd7b78193c76b831e866
Jessica Lima <jessikurt@gmail.com>
2023-07-17 17:39:25 -0300

    refatored pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -2,7 +2,76 @@ import React, { useState, useEffect } from "react";
 
 import { Container, Btn, Button, BtnArrow } from "./styles";
 
-const Pagination = ({ handleChange }) => {
+const Pagination = ({
+  handleChange,
+  limitePages,
+  countPages,
+  setCurrentPage,
+  currentPage,
+  getCurrentPage,
+}) => {
+  const [selectedPage, setSelectedPage] = useState(1);
+  const totalPages = Math.ceil(countPages / limitePages);
+
+
+  
+
+  function changePageNext() {
+    currentPage >= totalPages
+    ? alert("nao da pra mudar")
+    : setCurrentPage(currentPage + 1)
+  }
+
+  useEffect(() => {});
+
+  function changePagePrevious() {
+    currentPage <= 0
+      ? alert("nao da pra mudar")
+      : setCurrentPage(currentPage - 1);
+  }
+
+  function handleCurrentPage(page) {
+    getCurrentPage(Number(page));
+  }
+
+  return (
+    <Container>
+      <BtnArrow onClick={changePagePrevious}> &laquo; </BtnArrow>
+      {Array.from(Array(totalPages), (_, index) => {
+        return (
+          <Button
+            key={index}
+            selected={index === currentPage}
+            onClick={(e) => handleCurrentPage(e.target.textContent)}
+          >
+            <span>{index + 1}</span>
+          </Button>
+        );
+      })}
+      <BtnArrow onClick={() => changePageNext()}> &raquo; </BtnArrow>
+    </Container>
+  );
+};
+
+export default Pagination;
+
+/*<BtnArrow onClick={() => changePagePrevious()}> &laquo; </BtnArrow>
+      {pageNumbers.map((item, key) => {
+        return (
+          <Button
+            key={item.id}
+            selected={item.id === selectedPage}
+            onClick={() => {
+              setSelectedPage(item.id);
+            }}
+          >
+            <span key={key}>{item.id}</span>
+          </Button>
+        );
+      })}
+      <BtnArrow onClick={() => changePageNext()}> &raquo; </BtnArrow>*/
+
+/*
   const [selectedPage, setSelectedPage] = useState(0);
 
   const pageNumbers = [
@@ -32,29 +101,4 @@ const Pagination = ({ handleChange }) => {
     selectedPage <= 1
       ? alert("nao da pra mudar")
       : setSelectedPage(selectedPage - 1);
-  }
-
-  return (
-    <Container>
-      <Btn>
-        <BtnArrow onClick={() => changePagePrevious()}> &laquo; </BtnArrow>
-        {pageNumbers.map((item, key) => {
-          return (
-            <Button
-              key={item.id}
-              selected={item.id === selectedPage}
-              onClick={() => {
-                setSelectedPage(item.id);
-              }}
-            >
-              <span key={key}>{item.id}</span>
-            </Button>
-          );
-        })}
-        <BtnArrow onClick={() => changePageNext()}> &raquo; </BtnArrow>
-      </Btn>
-    </Container>
-  );
-};
-
-export default Pagination;
+  }*/
--- src/components/Pagination/styles.js
+++ src/components/Pagination/styles.js
@@ -3,28 +3,26 @@ import media from "styled-media-query";
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 3.5rem 0 5rem 0;
-`;
-
-export const Btn = styled.div`
-  display: flex;
+  margin: 2rem 0;
 `;
 
 export const Button = styled.button`
   background: ${({ selected }) => (selected ? "#F1245A" : "#F3C7CE")};
-  padding: 15px;
   color: ${({ selected }) => (selected ? "#FFE9F3" : "#F1245A")};
-  cursor: pointer;
   border: 0;
-  width: 3.5rem;
-  height: 3.5rem;
-  margin: 2px;
   border-radius: 4px;
   font-family: RobotoRegular;
   font-weight: bold;
+
+  width: 2.5rem;
+  height: 3rem;
+  margin: 0.1rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   &:hover {
     background-color: #f1245a;
@@ -33,9 +31,8 @@ export const Button = styled.button`
   }
 
   ${media.lessThan("medium")`
-    padding: 5px;
-    width: 1.6rem;
-    height: 1.6rem;
+    width: 1.5rem;
+    height: 1.5rem;
   `}
 `;
 
@@ -45,10 +42,13 @@ export const BtnArrow = styled.button`
   border: 0;
   background: none;
   color: #f1245a;
-  width: 3.5rem;
-  height: 3.5rem;
+  width: 2.5rem;
+  height: 3rem;
   margin: 2px;
   border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   
   &:hover {
     background-color: pink;
@@ -57,7 +57,7 @@ export const BtnArrow = styled.button`
 
   ${media.lessThan("medium")`
     padding: 4px;
-    width: 1.2rem;
-    height: 1.2rem;
+    width: 1rem;
+    height: 1rem;
   `}
 `;
