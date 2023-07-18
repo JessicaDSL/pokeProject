62c3836bf1a164c9e46d38a872facf6dc5031990
Jessica Lima <jessikurt@gmail.com>
2023-07-18 00:22:30 -0300

    change media query

--- src/components/PokeList/styles.js
+++ src/components/PokeList/styles.js
@@ -4,7 +4,7 @@ import media from "styled-media-query";
 export const Container = styled.div`
   display: flex;
   justify-content: space-between;
-  margin: 60px 0;
+  margin: 2rem 0 0 0;
 `;
 
 export const List = styled.div`
@@ -14,18 +14,18 @@ export const List = styled.div`
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 120px 120px 120px 120px 120px;
 
-  @media (max-width: 750px) {
+  @media (max-width: 650px) {
     display: grid;
     grid-gap: 1.5rem;
     grid-template-columns: 1fr;
     grid-template-rows: 120px;
   }
 
-  @media (min-width: 750px) and (max-width: 768px) {
+  @media (min-width: 651px) and (max-width: 768px) {
     display: grid;
     grid-gap: 0.5rem;
     grid-template-columns: 1fr 1fr;
-    grid-template-rows: 120px 120px;
+    grid-template-rows: 120px 120px; 
   }
 
   ${media.greaterThan("large")`
