13899aec0affe8dc42df7f7757b9ee4b6251843b
Jessica Lima <jessikurt@gmail.com>
2023-07-18 00:17:52 -0300

    change for dual quote

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 import { Background } from "./styles";
-import PokeImageRight from '../../assets/image/pokelogo1.png'
-import PokeImageLeft from '../../assets/image/pokeImageleft.png'
+import PokeImageRight from "../../assets/image/pokelogo1.png";
+import PokeImageLeft from "../../assets/image/pokeImageleft.png";
 
 const Header = () => {
   return (
     <Background>
       <img src={PokeImageLeft} alt="icone de um pokemon" />
-        <h2>PokeJessy</h2>
+      <h2>PokeJessy</h2>
       <img src={PokeImageRight} alt="icone de um pokemon" />
     </Background>
   );
